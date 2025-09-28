ac924a6a7fb3fadde8534d6d6067a85a7bb63e5c
Parsamr2004 <mr.parsa.dehini.2004@gmail.com>
2025-09-28 11:51:35 -0700

    my first change

--- our-testproject/src/Components/TeamList.jsx
+++ our-testproject/src/Components/TeamList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function TeamList() {
   const [show, setShow] = useState(false);
-  const [team, setTeam] = useState(["Soha", "maryam"]);
+  const [team, setTeam] = useState(["Soha", "maryam", "parsa"]);
   const [name, setName] = useState("");
 
   const toggleShow = () => setShow(!show);
